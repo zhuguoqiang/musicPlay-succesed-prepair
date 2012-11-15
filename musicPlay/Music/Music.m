fef8c8e7cd3dca1a9c40bb639b0f729f91b45a9e
zhuguoqiang <zhuguoqiang2010@163.com>
2012-11-15 14:43:18 +0800

    Update musicPlay/Music/Music.m
    delete httpp

--- musicPlay/Music/Music.m
+++ musicPlay/Music/Music.m
@@ -12,7 +12,7 @@
 #import <AudioToolbox/AudioToolbox.h>
 
 @implementation Music
-@synthesize url,name,album,artist,image,number,recently,love,lrcpath,httpp;
+@synthesize url,name,album,artist,image,number,recently,love,lrcpath;
 /*
 -(id)init{
     self=[super init];
