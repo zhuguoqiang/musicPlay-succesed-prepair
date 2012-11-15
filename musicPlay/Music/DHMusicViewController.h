feb20ae29d3d6dce78a55dc6fdf7cf87c262b4c8
zhuguoqiang <zhuguoqiang_2010@163.com>
2012-11-15 14:47:28 +0800

    add two file
    HDMusicVC

---
+++ musicPlay/Music/DHMusicViewController.h
@@ -0,0 +1,13 @@
+//
+//  DHMusicViewController.h
+//  musicPlay
+//
+//  Created by 朱国强 on 12-11-15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHMusicViewController : UIViewController
+
+@end
